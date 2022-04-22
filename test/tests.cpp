4c6766f7580c257a32b66243f73770f96902aaa8
Marek Forys <m.forys@tricentis.com>
2022-04-22 12:26:28 +0200

    First test works - MacOS
    Signed-off-by: Marek Forys <m.forys@tricentis.com>

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,9 +1,14 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+#include "../common/common.h"
+
+
+TEST(zSignTest, GetCanonicalizePath) {
+  string result = GetCanonicalizePath("just_file_name.extension");
+  EXPECT_EQ("/Users/marek/_repos/zsign_my_fork/zsign/test/just_file_name.extension", result);
+  result = GetCanonicalizePath("../another_file_name.ext");
+  EXPECT_EQ("/Users/marek/_repos/zsign_my_fork/zsign/test/../another_file_name.ext", result);
+  result = GetCanonicalizePath("some_folder/yet_another_file_name.ext");
+  EXPECT_EQ("/Users/marek/_repos/zsign_my_fork/zsign/test/some_folder/yet_another_file_name.ext", result);
+  //EXPECT_TRUE(false) << "result: " << result << std::endl;
 }
