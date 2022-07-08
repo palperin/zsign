9bb5683eea85f48ec9a224dfafc64ca4607b78d9
Pavel Alperin <p.alperin@tricentis.com>
2022-07-08 07:03:36 -0500

    fixup: copy lib files from openssl to local /lib in both cases of openssl path
    Signed-off-by: Pavel Alperin <p.alperin@tricentis.com>

--- INSTALL.sh
+++ INSTALL.sh
@@ -13,9 +13,13 @@ CmakeV=3.21.3
 	# Dependencies 
 	brew install zip unzip &&
 	brew install openssl cmake
+    
+    test -d "/usr/local/opt/openssl@3" &&
+        mkdir lib && cp /usr/local/opt/openssl@3/lib/libssl.dylib ./lib/ && cp /usr/local/opt/openssl@3/lib/libcrypto.dylib ./lib/
+
     test -d "/opt/homebrew/opt/" &&
-        mkdir -p /usr/local/opt/ && sudo cp -an /opt/homebrew/opt/openssl* /usr/local/opt/
-        mkdir lib && cp /usr/local/opt/lib/libssl.dylib ./lib/ && cp /usr/local/opt/lib/libcrypto.dylib ./lib/
+        mkdir -p /usr/local/opt/ && sudo cp -an /opt/homebrew/opt/openssl* /usr/local/opt/ &&
+        mkdir lib && cp /opt/homebrew/opt/openssl@3/lib/libssl.dylib ./lib/ && cp /opt/homebrew/opt/openssl@3/lib/libcrypto.dylib ./lib/
 
 # Compile zsign usign cmake
 mkdir build; cd build &&
